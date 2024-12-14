8540435cf1b1bc2a01292dec17ddf5dba8cfd7b4
Luhan Salimena <luhan@beepayapp.com.br>
2024-12-14 10:52:11 -0300

    Add routes for product data retrieval with various loading techniques and caching

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,104 @@
 <?php
 
+use App\Models\Product;
+use Illuminate\Support\Benchmark;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    Benchmark::dd([ 
+        'Eloquent' => fn() => Product::get(),
+        'Laravel Raw' => fn() => DB::table('products')->get(),
+    ], 25);
+});
+
+Route::get('/lazy', function () {
+
+    $produtos = Product::all();
+    
+    foreach ($produtos as $produto) {
+        echo $produto->photo->id;
+    }
+
+    return '';
+});
+
+Route::get('/eager', function () {
+
+    $produtos = Product::with('photo')->get();
+
+    // $produtos = Product::all();
+    // $produtos->load('photo');
+
+    foreach ($produtos as $produto) {
+        echo $produto->photo->id;
+    }
+
+    return '';
+});
+
+
+Route::get('/join', function () {
+
+    $produtos = Product::
+        select('products.*', 'photos.id as photo_id')
+        ->join('photos', 'products.id', '=', 'photos.product_id')->get();
+
+    foreach ($produtos as $produto) {
+        echo $produto->photo_id;
+    }
+
+    return '';
+});
+
+
+Route::get('/eager-select', function () {
+
+    $produtos = Product::with([
+        'photo' => function ($query) {
+            $query->select('id', 'name', 'product_id');
+        }
+    ])->get();
+
+    foreach ($produtos as $produto) {
+        echo $produto->photo->id;
+    }
+
+    return '';
+});
+
+Route::get('/paginate', function () {
+
+    $produtos = Product::paginate(50);
+
+    foreach ($produtos as $produto) {
+        echo $produto->id;
+    }
+
+    return '';
+});
+
+Route::get('/chunk', function () {
+
+    Product::chunk(100, function ($produtos) {
+        foreach ($produtos as $produto) {
+            // Processa cada bloco de 100 produtos
+            echo $produto->id;
+        }
+    });
+
+    return '';
+});
+
+Route::get('/cache', function () {
+
+    $produtos = Cache::remember('produtos_ativos', 60, function () {
+        return Product::get();
+    });
+
+    foreach ($produtos as $produto) {
+        echo $produto->id;
+    }
+
+    return '';
 });
